07b0080dabad05ceafcb950b3c8bad1481a1a331
liziyi0914 <liziyi0914@gmail.com>
2024-05-24 04:46:27 +0800

    feat(app): 新增Android自动构建

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,9 +1,12 @@
+import java.text.SimpleDateFormat
+import java.util.Date
 import java.util.Properties
 
 plugins {
     id("com.android.application")
     id("kotlin-android")
     id("dev.flutter.flutter-gradle-plugin")
+    id("com.github.triplet.play") version "3.9.0"
 }
 
 val localProperties = Properties()
@@ -13,13 +16,13 @@ if (localPropertiesFile.exists()) {
 }
 
 val flutterVersionCode = localProperties.getProperty("flutter.versionCode") ?: "1"
-
 val flutterVersionName = localProperties.getProperty("flutter.versionName") ?: "1.0"
+
+val projectName = "GamingEpochs"
+val buildTime: String = SimpleDateFormat("yyMMdd.HHmmss").format(Date())
 
 android {
     namespace = "com.liziyi0914.gamingepochs"
-//    compileSdk = flutter.compileSdkVersion
-//    ndkVersion = flutter.ndkVersion
     compileSdk = project.flutter.getProperty("compileSdkVersion") as Int?
     ndkVersion = project.flutter.getProperty("ndkVersion") as String?
 
@@ -39,19 +42,44 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.liziyi0914.gamingepochs"
-//        // You can update the following values to match your application needs.
-//        // For more information, see: https://docs.flutter.dev/deployment/android#reviewing-the-gradle-build-configuration.
-//        minSdkVersion flutter.minSdkVersion
-//        targetSdkVersion flutter.targetSdkVersion
-//        versionCode flutterVersionCode.toInteger()
-//        versionName flutterVersionName
 
         minSdk = project.flutter.getProperty("minSdkVersion") as Int?
         targetSdk = project.flutter.getProperty("targetSdkVersion") as Int?
         versionCode = flutterVersionCode.toInt()
         versionName = flutterVersionName
+
+        buildConfigField("String", "BUILD_TIME", "\"${buildTime}\"")
+
+        setProperty("archivesBaseName", "${projectName}-v${versionName}")
+    }
+
+    signingConfigs {
+        create("upload") {
+            storeFile = rootProject.file("upload.jks")
+            storePassword = System.getenv("KEYSTORE_PASSWORD")
+            keyAlias = "upload"
+            keyPassword = System.getenv("KEY_PASSWORD")
+        }
+    }
+
+    productFlavors {
+        flavorDimensions += "vendor"
+        create("common") {
+            dimension = "vendor"
+            manifestPlaceholders["CHANNEL"] = "Common"
+            manifestPlaceholders["JPUSH_CHANNEL"] = "Common"
+        }
+        create("google") {
+            dimension = "vendor"
+            manifestPlaceholders["CHANNEL"] = "Google"
+            manifestPlaceholders["JPUSH_CHANNEL"] = "Google"
+        }
+        create("huawei") {
+            dimension = "vendor"
+            manifestPlaceholders["CHANNEL"] = "Huawei"
+            manifestPlaceholders["JPUSH_CHANNEL"] = "Huawei"
+        }
     }
 
     buildTypes {
@@ -59,9 +87,32 @@ android {
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
 //            signingConfig signingConfigs.debug
+            isMinifyEnabled = true
+            signingConfig = signingConfigs.getByName("upload")
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+
+    playConfigs {
+        register("googleRelease") {
+            enabled.set(true)
+        }
+    }
+
+    applicationVariants.all {
+        outputs.all {
+            if (this is com.android.build.gradle.internal.api.BaseVariantOutputImpl) {
+                val suffix = outputFileName.split(".").last()
+                outputFileName = "${projectName}-v${versionName}-${flavorName}-${buildType.name}.${suffix}"
+            }
         }
     }
 }
+
+play {
+    enabled.set(false)
+}
+
 //
 //flutter {
 //    source '../..'
@@ -75,5 +126,5 @@ android {
 project.flutter.source = "../.."
 
 dependencies {
-    implementation("cn.hutool:hutool-all:5.8.25")
+    implementation("cn.hutool:hutool-all:5.8.26")
 }
